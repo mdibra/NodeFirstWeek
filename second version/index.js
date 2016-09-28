080cf2ee925b9b3bd0c0781a4ff1792de3beb7a4
Mohammad <md_ibra@hotmail.com>
2016-09-28 13:41:12 +0200

    Add files via upload

---
+++ second version/index.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var fs = require('fs');
+var port = 8080;
+var state = 10;
+
+function stateNumber(response) {
+	response.writeHead(200,{'Content-Type':'text/html'});
+	response.write('<h1>State is '+(state=10)+'</h1>');
+	console.log(state)
+	response.end();
+}
+
+function addNumber(response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('<h1>State is '+(state=state+1)+'</h1>');
+	console.log(state)
+	response.end()
+}
+function removeNumber(response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('<h1>State is '+(state=state-1)+'</h1>');
+	console.log(state)
+	response.end()
+}
+function send404(response) {
+	response.writeHead(404,{'Content-Type': 'text/html'});
+	response.write('404,There is an error, can\'t load the data');
+}
+var server = http.createServer(function(request,response){
+	
+	if (request.url === '/' || request.url === '/state' || request.url === '/reset') {
+		stateNumber(response);
+	}
+	
+	else if (request.url==='/add') {
+		addNumber(response);
+	} 
+	
+	else if (request.url === '/remove') {
+		removeNumber(response);
+	}
+	
+	else {
+		send404(response);
+	}
+});
+
+server.listen(port, function(error){
+	if (error) {
+		console.log(error)
+	} else {console.log('listening on ', port)}
+})

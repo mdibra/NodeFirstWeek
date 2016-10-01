bcb82c63fdd0c17015c5a6a83a4985f02bb2d636
Mohammad <md_ibra@hotmail.com>
2016-10-01 16:17:54 +0200

    Adding som functions

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 var http = require('http');
 var port = 8080;
 var state = 10;
+
+function stateNumber(response) {
+	response.writeHead(200,{'Content-Type':'text/html'});
+	response.write('<h1>State is '+(state=10)+'</h1>');
+	console.log(state)
+	response.end();
+}
+
+function addNumber(response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('<h1>State is '+(state=state+1)+'</h1>');
+	console.log(state)
+	response.end()
+}
+function removeNumber(response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('<h1>State is '+(state=state-1)+'</h1>');
+	console.log(state)
+	response.end()
+}
+function send404(response) {
+	response.writeHead(404,{'Content-Type': 'text/html'});
+	response.write('404,There is an error, can\'t load the data');
+}
+
 var server = http.createServer(function(request,response){
 	
 	if (request.url === '/' || request.url === '/state' || request.url === '/reset') {
-		response.writeHead(200,{'Content-Type':'text/html'});
-		response.write('<h1>State is '+(state=10)+'</h1>');
-		console.log(state)
-		response.end();
+		stateNumber(response);
 	}
 	
 	else if (request.url==='/add') {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('<h1>State is '+(state=state+1)+'</h1>');
-		console.log(state)
-		response.end()
+		addNumber(response);
 	} 
 	
 	else if (request.url === '/subtract') {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('<h1>State is '+(state=state-1)+'</h1>');
-		console.log(state)
-		response.end()
+		removeNumber(response);
 	}
 	
 	else {
-		response.writeHead(404,{'Content-Type': 'text/html'});
-		response.write('404,There is an error, can\'t load the data');
+		send404(response);
 	}
 });
 
